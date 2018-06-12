e16085891730d209d712079fe6c69748716a04a9
murtaza <murtaza.nathani@tenpearls.com>
2018-06-12 12:33:40 +0500

    Added dockerfile for docker.cloud and rancher testing
    Signed-off-by: murtaza <murtaza.nathani@tenpearls.com>

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+
 app.listen(8000, function () {
+  console.log("hi!");
   console.log('Example app listening on port 8000!');
 })
